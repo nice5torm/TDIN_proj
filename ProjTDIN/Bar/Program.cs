327aebbab5957c624d6266326e408dae594597e6
nice5torm <marianadguimaraes@hotmail.com>
2019-04-04 16:24:37 +0100

    inicio projeto1

---
+++ ProjTDIN/Bar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KitchenBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemotingConfiguration.Configure("KitchenBar.exe.config", false);
+        }
+    }
+}
---
+++ ProjTDIN/DinningRoom/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DinningRoom
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            RemotingConfiguration.Configure("DinningRoom.exe.config", false);
+        }
+    }
+}
