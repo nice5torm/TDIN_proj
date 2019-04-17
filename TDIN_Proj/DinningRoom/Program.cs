8117b7873b925a22e9d28dc64df54e54ea5cb067
nice5torm <marianadguimaraes@hotmail.com>
2019-04-17 17:58:09 +0100

    choice kitchen/bar

--- TDIN_Proj/DinningRoom/Program.cs
+++ TDIN_Proj/DinningRoom/Program.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Models.Class1;
 
 namespace DinningRoom
 {
     static class Program
     {
+        public static IManagement ListServer; //?
+
         /// <summary>
         /// Ponto de entrada principal para o aplicativo.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            ListServer = (IManagement)Activator.GetObject(typeof(IManagement), "tcp://localhost:9000/Server/OrdersServer"); //?
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
---
+++ TDIN_Proj/KitchenBar/Choice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KitchenBar
+{
+    public partial class Choice : Form
+    {
+        public Choice()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 formBar = new Form1(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 formKitchen = new Form1(0);
+        }
+    }
+}
