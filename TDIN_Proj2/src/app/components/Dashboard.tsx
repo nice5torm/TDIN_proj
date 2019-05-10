b93aab1234dcf5693df9d07cff15148b825413fa
nice5torm <marianadguimaraes@hotmail.com>
2019-05-10 20:34:12 +0100

    setup com react

---
+++ TDIN_Proj2/src/app/components/Dashboard.tsx
@@ -0,0 +1,5 @@
+﻿import * as React from 'react';
+
+export const Dashboard = () => {
+    return <div>Hello Mom!</div>;
+};
---
+++ TDIN_Proj2/src/app/renderer.tsx
@@ -0,0 +1,5 @@
+﻿import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import {Dashboard} from "./components/Dashboard";
+
+ReactDOM.render(<Dashboard />, document.getElementById('renderer'));
