365937c5710305e60863c6fb7d6e0a2d041cecba
nice5torm <marianadguimaraes@hotmail.com>
2019-05-10 19:42:32 +0100

    first setup

--- TDIN_Proj2/src/app/renderer.tsx
+++
@@ -1,5 +0,0 @@
-import * as ReactDOM from 'react-dom';
-import * as React from 'react';
-import {Dashboard} from "./components/Dashboard";
-
-ReactDOM.render(<Dashboard />, document.getElementById('renderer'));
