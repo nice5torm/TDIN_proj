e690071fa8be987099e6e86d1e2cf629a15a6321
Eduardo Leite <embleite@gmail.com>
2019-05-18 01:04:47 +0100

    :)

--- TDIN2/WebApp/Startup.cs
+++ TDIN2/WebApp/Startup.cs
@@ -22,7 +22,13 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+			services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+			
+			services.AddCors();
+			
+            services.AddMvc()
+                .AddJsonOptions(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +47,12 @@ namespace WebApp
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+			app.UseCors(builder => builder
+               .AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials());
 
             app.UseStaticFiles();
 
